b83b1d2db054dd47922f53a108ac49aea1b986b0
ginsederp <ginsederp@derpymail.org>
2015-06-18 20:49:13 +0800

    Created Int3 struct. It's like Vector3, but with ints.

---
+++ Int3.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ginsederp.miniblocks
+{
+  public struct Int3
+  {
+    public int x;
+    public int y;
+    public int z;
+
+    public Int3( int _x, int _y, int _z )
+    {
+      x = _x;
+      y = _y;
+      z = _z;
+
+      return;
+    }
+  }
+}
