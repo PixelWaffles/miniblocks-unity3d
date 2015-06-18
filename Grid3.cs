caa4a6d9a38691d1f2db5e921935bfc6c03ea0b7
ginsederp <ginsederp@derpymail.org>
2015-06-18 20:59:17 +0800

    Implemented AllocateGrid() function.

--- Grid3.cs
+++ Grid3.cs
@@ -6,8 +6,39 @@ namespace ginsederp.miniblocks
 {
   public class Grid3<T>
   {
-    public List<T> x = new List<T>();
-    public List<T> y = new List<T>();
-    public List<T> z = new List<T>();
+    public List<T> xGrid = new List<T>();
+    public List<T> yGrid = new List<T>();
+    public List<T> zGrid = new List<T>();
+
+    public void Add( Int3 _begin, Int3 _size )
+    {
+      AllocateGrid( _begin, _size );
+
+      Debug.Log( "x alloc: " + xGrid.Count );
+      Debug.Log( "y alloc: " + yGrid.Count );
+      Debug.Log( "z alloc: " + zGrid.Count );
+    }
+
+    public void AllocateGrid( Int3 _begin, Int3 _size )
+    {
+      Int3 allocateArea;
+      allocateArea.x = _begin.x + _size.x;
+      allocateArea.y = _begin.y + _size.y;
+      allocateArea.z = _begin.z + _size.z;
+
+      if( xGrid.Count <= allocateArea.x ) {
+        xGrid.AddRange( new T[ allocateArea.x - ( xGrid.Count - 1 ) ] ); 
+      }
+
+      if( yGrid.Count <= allocateArea.y ) {
+        yGrid.AddRange( new T[ allocateArea.y - ( yGrid.Count - 1 ) ] ); 
+      }
+
+      if( zGrid.Count <= allocateArea.z ) {
+        zGrid.AddRange( new T[ allocateArea.z - ( zGrid.Count - 1 ) ] ); 
+      }
+
+      return;
+    }
   }
 }
--- Miniblocks.cs
+++ Miniblocks.cs
@@ -6,5 +6,11 @@ namespace ginsederp.miniblocks
   public class Miniblocks : MonoBehaviour
   {
     public Grid3<int> blockIdMap = new Grid3<int>();
+
+    void Start()
+    {
+      blockIdMap.Add( new Int3( 0, 0, 0 ), new Int3( 9, 19, 29 ) );
+      return;
+    }
   }
 }
