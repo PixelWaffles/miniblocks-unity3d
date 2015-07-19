8d45521d138a0b20b2dc21afd0d12eca637f537b
ginsederp <ginsederp@derpymail.org>
2015-07-20 02:13:50 +0800

    Implemented back facing marching square mesh generation based on the blockIdMap.

--- Miniblocks.cs
+++ Miniblocks.cs
@@ -19,8 +19,6 @@ namespace ginsederp.miniblocks
       meshRenderer = ( meshRenderer != null ) ? meshRenderer : gameObject.AddComponent<MeshRenderer>();
       meshFilter = ( meshFilter != null ) ? meshFilter : gameObject.AddComponent<MeshFilter>();
 
-      GenerateMesh();
-
       return;
     }
 
@@ -31,9 +29,20 @@ namespace ginsederp.miniblocks
       List<Vector3> verts = new List<Vector3>();
       List<int> tris = new List<int>();
 
-      // Test Generate Face
-      tris.AddRange( BuildFaceTris( verts.Count, false ) );
-      verts.AddRange( BuildFaceVerts( Vector3.zero, Vector3.up, Vector3.right ) );
+      for( int xi = 0; xi < blockIdMap.grid.GetLength( 0 ); xi++ ) {
+        for( int yi = 0; yi < blockIdMap.grid.GetLength( 1 ); yi++ ) {
+          for( int zi = 0; zi < blockIdMap.grid.GetLength( 2 ); zi++ ) {
+            if( blockIdMap.grid[ xi, yi, zi ] == 0 ) {
+              continue;
+            }
+
+            // Build Back Faces
+            tris.AddRange( BuildFaceTris( verts.Count, false ) );
+            verts.AddRange( BuildFaceVerts( new Vector3( xi, yi, zi ), Vector3.up, Vector3.right ) );
+
+          }
+        }
+      }
 
       mesh.vertices = verts.ToArray();
       mesh.triangles = tris.ToArray();
@@ -89,9 +98,9 @@ namespace ginsederp.miniblocks
     {
       InitComponents();
 
-      blockIdMap.Add( 1, new Int3( 3, 4, 5 ), new Int3( 10, 20, 30 ) );
-      blockIdMap.Add( 2, new Int3( 2, 3, 4 ), new Int3( 15, 22, 34 ) );
-      blockIdMap.Remove( new Int3( 0, 0, 0 ), new Int3( 10, 10, 10 ) );
+      blockIdMap.Add( 1, new Int3( 0, 0, 0 ), new Int3( 10, 10, 10 ) );
+      GenerateMesh();
+
       return;
     }
   }
