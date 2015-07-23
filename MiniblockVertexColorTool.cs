568d99bc3a250b973d9dfacceb7531d531f83ccf
ginsederp <ginsederp@derpymail.org>
2015-07-23 15:43:20 +0800

    Created MiniblockVertexColorTool stub.

---
+++ MiniblockVertexColorTool.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ginsederp.miniblocks
+{
+  public class MiniblockVertexColorTool : MonoBehaviour
+  {
+    [SerializeField] protected Color32[] colors;
+
+    void Start()
+    {
+      return;
+    }
+  }
+}
