da296ebcc737d17b7fddd69badca3e92135f2445
ginsederp <ginsederp@derpymail.org>
2015-08-03 18:32:58 +0800

    Re-added MiniblockRectangleTool array properties in inspector.

--- Editor/MiniblockRectangleToolEditor.cs
+++ Editor/MiniblockRectangleToolEditor.cs
@@ -6,21 +6,26 @@ namespace ginsederp.miniblocks
   [CustomEditor( typeof(MiniblockRectangleTool) )]
   public class MiniblockRectangleToolEditor : Editor
   {
-    protected SerializedObject blockRectToolSo;
+    protected SerializedObject so;
     protected MiniblockRectangleTool blockRectTool;
 
     void OnEnable()
     {
-      blockRectToolSo = new SerializedObject(target);
+      so = new SerializedObject(target);
       blockRectTool = (MiniblockRectangleTool)target;
       return;
     }
 
     public override void OnInspectorGUI()
     {
-      blockRectToolSo.Update();
-      //TODO GUI for consolidated tool array fields.
-      blockRectToolSo.ApplyModifiedProperties();
+      so.Update();
+
+      EditorGUILayout.PropertyField( so.FindProperty("toolType"), true );
+      EditorGUILayout.PropertyField( so.FindProperty("toolBlockId"), true );
+      EditorGUILayout.PropertyField( so.FindProperty("toolStart"), true );
+      EditorGUILayout.PropertyField( so.FindProperty("toolSize"), true );
+
+      so.ApplyModifiedProperties();
       return;
     }
 
