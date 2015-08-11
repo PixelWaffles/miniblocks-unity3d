fdcdb0db6686bce246c7e1a5577f6fd9f582fedf
ginsederp <ginsederp@derpymail.org>
2015-08-12 03:32:38 +0800

    Turned MiniblockVertexColorTool inspector array color field into a reorderableList.

--- Editor/MiniblockVertexColorToolEditor.cs
+++ Editor/MiniblockVertexColorToolEditor.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 
 namespace ginsederp.miniblocks
 {
@@ -9,10 +10,52 @@ namespace ginsederp.miniblocks
     protected SerializedObject so;
     protected MiniblockVertexColorTool colorTool;
 
+    protected ReorderableList reorderableList;
+
+    protected void ReorderableDrawElement( Rect _rect, int _index, bool _active, bool _focused )
+    {
+      SerializedProperty element = reorderableList.serializedProperty.GetArrayElementAtIndex( _index );
+      _rect.y += 2;
+
+      EditorGUI.LabelField(
+        new Rect( _rect.x, _rect.y, EditorGUIUtility.fieldWidth, EditorGUIUtility.singleLineHeight ),
+        _index.ToString()
+      );
+
+      EditorGUI.PropertyField(
+        new Rect( _rect.x + EditorGUIUtility.fieldWidth, _rect.y, _rect.width - EditorGUIUtility.fieldWidth, EditorGUIUtility.singleLineHeight ),
+        element,
+        GUIContent.none
+      );
+      return;
+    }
+
+    protected void ReorderableDrawHeader( Rect _rect )
+    {
+      EditorGUI.LabelField(
+        new Rect( _rect.x, _rect.y, EditorGUIUtility.fieldWidth, EditorGUIUtility.singleLineHeight ),
+       "Id" );
+
+      EditorGUI.LabelField(
+        new Rect( _rect.x + EditorGUIUtility.fieldWidth, _rect.y, _rect.width - EditorGUIUtility.fieldWidth, EditorGUIUtility.singleLineHeight ),
+       "Colors" );
+      return;
+    }
+
+    protected void SetupReorderableList()
+    {
+      reorderableList = new ReorderableList( so, so.FindProperty("idColors"), true, true, true, true );
+      reorderableList.drawElementCallback = ReorderableDrawElement;
+      reorderableList.drawHeaderCallback = ReorderableDrawHeader;
+      return;
+    }
+
     void OnEnable()
     {
       so = new SerializedObject(target);
       colorTool = (MiniblockVertexColorTool)target;
+
+      SetupReorderableList();
       return;
     }
 
@@ -20,7 +63,7 @@ namespace ginsederp.miniblocks
     {
       so.Update();
 
-      EditorGUILayout.PropertyField( so.FindProperty("idColors"), true );
+      reorderableList.DoLayoutList();
 
       if( so.ApplyModifiedProperties() ) {
         EditorHelper.GenerateMiniblocks( colorTool.gameObject );
